0f0ee64651b401bfc2b6e2cc2a5b6dbb21f6786c
Akayda1 <164392529+Akayda1@users.noreply.github.com>
2024-07-12 17:23:04 +0100

    Add files via upload

---
+++ Day 4.py
@@ -0,0 +1,40 @@
+#Randomisation
+# import random
+# random_integer = random.randint(1, 10)
+# print(random_integer) 
+
+# random_float = random.random() * 5
+# print(random_float) 
+
+# love_score = random.randint(1, 100)
+# print(f"Your love score is {love_score}") 
+
+# # Virtual coin toss program
+# random_side = random.randint(0,1 )
+# if random_side == 1:
+#     print("Heads")
+# else:
+#     print("Tails") 
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+ 
+
